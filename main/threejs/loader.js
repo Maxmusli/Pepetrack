7cea3c156407aff4ef51db4a65132999fdefa511
max <maxmus2223@gmail.com>
2019-11-27 11:41:21 -0800

    add loader function

--- main/threejs/loader.js
+++ main/threejs/loader.js
@@ -42,7 +42,7 @@ game.threejs.Loader = (opts) => {
   }
 }
 
-game.threeejs.Loader.prototype.load = (data) => {
+game.threejs.Loader.prototype.load = (data) => {
   for (let k in this.types) {
     if (k in data) {
       let size = 0;
@@ -79,3 +79,100 @@ game.threeejs.Loader.prototype.load = (data) => {
   this.progressCallback.call(this, this.progress);
 }
 
+game.threejs.Loader.prototype.updateState = (type, name, state) => {
+  if (!(type in this.types)) {
+    console.warn("Unkown loader type.");
+    return;
+  }
+
+  if (state === true) {
+    this.progress.remaining--;
+    this.progress.loaded++;
+    this.progressCallback.call(this, this.progress, type, name);
+  }
+
+  this.states[type][name] = state;
+
+  if (this.progress.loaded === this.progress.total) {
+    this.loadCallback.call(this);
+  }
+}
+
+game.threejs.Loader.prototype.get = (type, name) => {
+  if (!(type in this.types)) {
+    console.warn("Unkown loader type.");
+    return null;
+  }
+
+  if (!(name in this.data[type])) {
+    console.warn("Unkown file.");
+    return null;
+  }
+
+  return this.data[type][name];
+}
+
+game.threejs.Loader.prototype.loaded = (type, name) => {
+  if (!(type in this.types)) {
+    console.warn("Unkown loader type.");
+    return null;
+  }
+
+  if (!(name in this.data[type])) {
+    console.warn("Unkown file.");
+    return null;
+  }
+
+  return this.states[type][name];
+}
+
+game.threejs.Loader.prototype.loadTexture = (name, url) => {
+  this.updateState("textures", name, false);
+  this.data.textures[name] = THREE.ImageUtils.loadTexture(
+    url,
+    game.NONE,
+    () => {
+      self.updateState("textures", name, true);
+    },
+    () => {
+      self.errorCallback.call(self, name);
+    }
+  );
+}
+
+game.threejs.Loader.prototype.loadTextureCube = (name, url) {
+  let urls = [
+    url.replace("%1", "px"), url.replace("%1", "nx"),
+    url.replace("%1", "py"), url.replace("%1", "ny"),
+    url.replace("%1", "pz"), url.replace("%1", "nz")
+  ];
+
+  this.updateState("texturesCube", name, false);
+  this.data.texturesCube[name] = THREE.ImageUtils.loadTextureCube(
+    urls,
+    new THREE.CubeRefractionMapping(),
+    () => {
+      this.updateState("texturesCube", name, true);
+    }
+  );
+}
+
+game.threejs.Loader.prototype.loadGeometry = (name, url) => {
+  this.data.geometries[name] = null;
+  this.updateState("geometries", name, false);
+  this.jsonLoader.load(
+    url,
+    (a) => {
+      this.data.geometries[name] = a;
+      this.updateState("geometries", name, true);
+    }
+  );
+}
+
+game.threejs.Loader.prototype.loadAnalyser = (name, url) => {
+  
+}
+
+game.threejs.Loader.prototype.loadImage = (name, url) {
+
+}
