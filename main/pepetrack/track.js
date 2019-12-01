fb0fef77969563dd7fa50b5c4ecdce5bd2614935
max <maxmus2223@gmail.com>
2019-11-30 20:03:05 -0800

    add track

---
+++ main/pepetrack/track.js
@@ -0,0 +1,19 @@
+let pepetrack = pepetrack || {};
+pepetrack.game = pepetrack.game || {};
+// pepetrack.game.tracks = pepetrack.game.tracks || {};
+
+pepetrack.game.tracks = {
+  lib: null,
+  materials: {},
+  name: 'track',
+
+  checkpoints: {
+    list: [0, 1, 2],
+    start: 0,
+    last: 2
+  },
+
+  spawn: {
+    x: 
+  }
+}
