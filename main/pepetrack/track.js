e8ee4b93faf832400dc6789410a0a5dae23b19c9
max <maxmus2223@gmail.com>
2019-12-05 14:25:05 -0800

    add track geometries

--- main/pepetrack/track.js
+++ main/pepetrack/track.js
@@ -17,5 +17,32 @@ pepetrack.game.tracks = {
     x: -1134 * 2,
     y: 387,
     z: -443 * 2
+  },
+
+  spawnRotation: {
+    x: 0,
+    y: 0,
+    z: 0
+  },
+  
+  load: function(opts) {
+    this.lib = new pepetrack.threejs.Loader();
+
+    this.lib.load({
+      textures: {
+        'track.diffuse': "textures/tracks/diffuse.jpg",
+      },
+      geometries: {
+        'bonus.base': "geometries/bonus/base/base.json",
+        'booster': "geometries/booster/booster.json",
+        'ship.feisar': "geometries/ships/feisar/feisar.json",
+        'track.cityscape': "geometries/tracks/cityscape/track.json",
+        'track.cityscape.scrapers1': "geometries/tracks/cityscape/scrapers1.json",
+        'track.cityscape.scrapers2': "geometries/tracks/cityscape/scrapers2.json",
+        'track.cityscape.start': "geometries/tracks/cityscape/start.json",
+        'track.cityscape.start.banner': "geometries/tracks/cityscape/startbanner.json",
+        'track.cityscape.bonus.speed': "geometries/tracks/cityscape/bonus/speed.json"
+      }
+    })
   }
 }
